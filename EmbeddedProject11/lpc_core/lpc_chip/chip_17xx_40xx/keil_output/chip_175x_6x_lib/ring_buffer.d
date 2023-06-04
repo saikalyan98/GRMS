944c6b7f8174725b46fb8a4b8dfead55ea42f2cf
saikalyan98 <52596098+saikalyan98@users.noreply.github.com>
2023-06-04 23:14:11 +0530

    GRMS Ver 0.1

---
+++ EmbeddedProject11/lpc_core/lpc_chip/chip_17xx_40xx/keil_output/chip_175x_6x_lib/ring_buffer.d
@@ -0,0 +1,6 @@
+.\keil_output\chip_175x_6x_lib\ring_buffer.o: ..\..\lpc_chip\chip_common\ring_buffer.c
+.\keil_output\chip_175x_6x_lib\ring_buffer.o: C:\Keil_v5\ARM\ARMCCv506u6\Bin\..\include\string.h
+.\keil_output\chip_175x_6x_lib\ring_buffer.o: ..\..\lpc_chip\chip_common\ring_buffer.h
+.\keil_output\chip_175x_6x_lib\ring_buffer.o: ..\..\lpc_chip\chip_common\lpc_types.h
+.\keil_output\chip_175x_6x_lib\ring_buffer.o: C:\Keil_v5\ARM\ARMCCv506u6\Bin\..\include\stdint.h
+.\keil_output\chip_175x_6x_lib\ring_buffer.o: C:\Keil_v5\ARM\ARMCCv506u6\Bin\..\include\stdbool.h
